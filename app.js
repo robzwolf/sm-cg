d88db08620d57dffd390500e7c0221272cec5145
Robbie Jakob-Whitworth <mail@rjwhitworth.co.uk>
2018-03-20 16:59:43 +0000

    Spinning cube

--- app.js
+++ app.js
@@ -1,29 +1,5 @@
 // app.js
 
-var vertexShaderText = [
-    "precision mediump float;",
-    "",
-    "attribute vec2 vertPosition;",
-    "attribute vec3 vertColor;",
-    "varying vec3 fragColor;",
-    "",
-    "void main()",
-    "{",
-    "   fragColor = vertColor;",
-    "   gl_Position = vec4(vertPosition, 0.0, 1.0);",
-    "}"
-].join("\n");
-
-var fragmentShaderText = [
-    "precision mediump float;",
-    "",
-    "varying vec3 fragColor;",
-    "void main()",
-    "{",
-    "   gl_FragColor = vec4(fragColor, 1.0);",
-    "}"
-].join("\n");
-
 var initDemo = function() {
     console.log("Init demo called");
 
@@ -39,13 +15,18 @@ var initDemo = function() {
         alert("Your browser does not support WebGL.");
     }
 
-    // canvas.width = window.innerWidth;
-    // canvas.height = window.innerHeight;
-    //
-    // gl.viewport(0, 0, window.innerWidth, window.innerHeight);
+    // Scale the canvas to the size of the window
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    gl.viewport(0, 0, window.innerWidth, window.innerHeight);
 
+    // Initialise WebGL
     gl.clearColor(.75, .85, .8, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.enable(gl.DEPTH_TEST);
+    gl.enable(gl.CULL_FACE);
+    gl.frontFace(gl.CCW);
+    gl.cullFace(gl.BACK);
 
     // Create shaders
     var vertexShader = gl.createShader(gl.VERTEX_SHADER);
@@ -84,23 +65,93 @@ var initDemo = function() {
 
     // Create buffer
     var triangleVertices = [
-            // X, Y          R, G, B
-            0.0, 0.5,       1.0, 1.0, 0.0,
-            -0.5, -0.5,     0.7, 0.0, 1.0,
-            0.5, -0.5,      0.1, 1.0, 0.6
+            // X, Y, Z           R,   G,   B
+            0.0, 0.5, 0.0,       1.0, 1.0, 0.0,
+            -0.5, -0.5, 0.0,     0.7, 0.0, 1.0,
+            0.5, -0.5, 0.0,      0.1, 1.0, 0.6
     ];
 
-    var triangleVertexBufferObject = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBufferObject);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices), gl.STATIC_DRAW);
+    var cubeVertices = [
+        // X     Y     Z          R     G     B
+
+        // Top
+           -1.0,  1.0, -1.0,      0.5,  0.5,  0.5,
+           -1.0,  1.0,  1.0,      0.5,  0.5,  0.5,
+            1.0,  1.0,  1.0,      0.5,  0.5,  0.5,
+            1.0,  1.0, -1.0,      0.5,  0.5,  0.5,
+
+        // Left
+           -1.0,  1.0,  1.0,      0.75, 0.25, 0.5,
+           -1.0, -1.0,  1.0,      0.75, 0.25, 0.5,
+           -1.0, -1.0, -1.0,      0.75, 0.25, 0.5,
+           -1.0,  1.0, -1.0,      0.75, 0.25, 0.5,
+
+        // Right
+            1.0,  1.0,  1.0,      0.25, 0.25, 0.75,
+            1.0, -1.0,  1.0,      0.25, 0.25, 0.75,
+            1.0, -1.0, -1.0,      0.25, 0.25, 0.75,
+            1.0,  1.0, -1.0,      0.25, 0.25, 0.75,
+
+        // Front
+            1.0,  1.0,  1.0,      1.0,  0.0,  0.15,
+            1.0, -1.0,  1.0,      1.0,  0.0,  0.15,
+           -1.0, -1.0,  1.0,      1.0,  0.0,  0.15,
+           -1.0,  1.0,  1.0,      1.0,  0.0,  0.15,
+
+        // Back
+            1.0,  1.0, -1.0,      0.0,  1.0,  0.15,
+            1.0, -1.0, -1.0,      0.0,  1.0,  0.15,
+           -1.0, -1.0, -1.0,      0.0,  1.0,  0.15,
+           -1.0,  1.0, -1.0,      0.0,  1.0,  0.15,
+
+        // Bottom
+           -1.0, -1.0, -1.0,      0.5,  0.5,  1.0,
+           -1.0, -1.0,  1.0,      0.5,  0.5,  1.0,
+            1.0, -1.0,  1.0,      0.5,  0.5,  1.0,
+            1.0, -1.0, -1.0,      0.5,  0.5,  1.0
+    ]
+
+    var cubeIndices = [
+        // Top
+         0,  1,  2,
+         0,  2,  3,
+
+        // Left
+         5,  4,  6,
+         6,  4,  7,
+
+        // Right
+         8,  9, 10,
+         8, 10, 11,
+
+        // Front
+        13, 12, 14,
+        15, 14, 12,
+
+        // Back
+        16, 17, 18,
+        16, 18, 19,
+
+        // Bottom
+        21, 20, 22,
+        22, 20, 23
+    ]
+
+    var cubeVertexBufferObject = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexBufferObject);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(cubeVertices), gl.STATIC_DRAW);
+
+    var cubeIndexBufferObject = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeIndexBufferObject);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(cubeIndices), gl.STATIC_DRAW);
 
     var positionAttributeLocation = gl.getAttribLocation(program, "vertPosition");
     gl.vertexAttribPointer(
         positionAttributeLocation, // Attribute location
-        2, // Number of elements per attribute
+        3, // Number of elements per attribute
         gl.FLOAT, // Type of elements
         gl.FALSE,
-        5 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
+        6 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
         0 // Offset from the beginning of a single vertex to this attribute
     );
     var colorAttributeLocation = gl.getAttribLocation(program, "vertColor");
@@ -109,16 +160,56 @@ var initDemo = function() {
         3, // Number of elements per attribute,
         gl.FLOAT, // Type of elements
         gl.FALSE,
-        5 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
-        2 * Float32Array.BYTES_PER_ELEMENT // Offset from the beginning of a single vertex to this attribute
+        6 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
+        3 * Float32Array.BYTES_PER_ELEMENT // Offset from the beginning of a single vertex to this attribute
     );
 
     gl.enableVertexAttribArray(positionAttributeLocation);
     gl.enableVertexAttribArray(colorAttributeLocation);
 
-    // Main render loop
+    // Tell OpenGL state machine which program should be active
     gl.useProgram(program);
-    gl.drawArrays(gl.TRIANGLES, 0, 3);
+
+    var matWorldUniformLocation = gl.getUniformLocation(program, "mWorld");
+    var matViewUniformLocation = gl.getUniformLocation(program, "mView");
+    var matProjUniformLocation = gl.getUniformLocation(program, "mProj");
+
+    var worldMatrix = new Float32Array(16);
+    var viewMatrix = new Float32Array(16);
+    var projMatrix = new Float32Array(16);
+    mat4.identity(worldMatrix);
+    mat4.lookAt(viewMatrix, [0, 0, -8], [0, 0, 0], [0, 1, 0]);
+    mat4.perspective(projMatrix, glMatrix.toRadian(45), canvas.width / canvas.height, 0.1, 1000.0);
+
+    gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
+    gl.uniformMatrix4fv(matViewUniformLocation, gl.FALSE, viewMatrix);
+    gl.uniformMatrix4fv(matProjUniformLocation, gl.FALSE, projMatrix);
+
+    var xRotationMatrix = new Float32Array(16);
+    var yRotationMatrix = new Float32Array(16);
+
+    // Main render loop
+    var identityMatrix = new Float32Array(16);
+    mat4.identity(identityMatrix);
+    var angle = 0;
+    var loop = function() {
+        // One full rotation every six seconds
+        angle = performance.now() / 1000 / 6 * 2 * Math.PI;
+        mat4.rotate(yRotationMatrix, identityMatrix, angle, [0, 1, 0]);
+        mat4.rotate(xRotationMatrix, identityMatrix, angle / 4, [1, 0, 0]);
+        mat4.mul(worldMatrix, yRotationMatrix, xRotationMatrix);
+        gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
+
+        gl.clearColor(.75, .85, .8, 1.0);
+        gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
+
+        gl.drawElements(gl.TRIANGLES, cubeIndices.length, gl.UNSIGNED_SHORT, 0);
+
+        requestAnimationFrame(loop);
+    }
+    requestAnimationFrame(loop);
+
+
 
 
 }
